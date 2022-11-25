affe35be89e8e51ec813071e14f0033bbd6b7269
Sergey Evseev <sevseev@gmail.com>
2022-11-25 04:12:00 +0300

    Font

--- WindowsFormsGDI_Lesson/Form1.cs
+++ WindowsFormsGDI_Lesson/Form1.cs
@@ -22,25 +22,12 @@ namespace WindowsFormsGDI_Lesson
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics; //объект Graphics использует методы для отображения
-            //изображения на экране
-            //принимает X, Y, width, height
-            Rectangle rect = new Rectangle(20, 20, 200, 40);
-            //градиентная кисть для смешивания двух цветов
-            LinearGradientBrush lgBrush = new LinearGradientBrush(
-                rect, Color.Red, Color.Green, 0.0f, true);
-            //метод объекта Graphics принимает объекты Brush и Rectangle
-            g.FillRectangle(lgBrush, rect);
+                                     //изображения на экране
 
-            Rectangle rect2 = new Rectangle(20, 80, 200, 40);
-            //HatchBrush заливает форму каким-либо узором
-            HatchBrush hBrush = new HatchBrush(HatchStyle.DottedGrid, Color.AliceBlue);
-            g.FillRectangle(hBrush, rect2);
-
-            Rectangle rect3 = new Rectangle(20, 140, 200, 40);
-            //TextureBrush заливает объекты растровым изображением
-            TextureBrush tBrush = new TextureBrush(new Bitmap("grape.png"));
-            g.FillRectangle(tBrush, rect3);
-
+            Font f = new Font("Verdana", 14, FontStyle.Bold | FontStyle.Italic);
+            g.DrawString("Hello Font", f, Brushes.Blue, 30, 50);        
+            g.DrawString("Hello Font", f, Brushes.Blue, 30, 70);        
+            
             g.Dispose();
         }
     }
