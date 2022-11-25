8f7e191822abc602b8a251c7a73e53c9df1dd2df
Sergey Evseev <sevseev@gmail.com>
2022-11-25 03:13:05 +0300

    Add project files.

---
+++ WindowsFormsGDI_Lesson/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsGDI_Lesson
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Rectangle rect = new Rectangle(20, 20, 200, 40);
+
+        }
+    }
+}
