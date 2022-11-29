ee2239fedabefe57f3cc03ac99723ae36e53b3ba
Sergey Evseev <sevseev@gmail.com>
2022-11-30 01:35:30 +0300

    FillRegion

--- REGION/Form1.cs
+++ REGION/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,8 +16,9 @@ namespace REGION
         public Form1()
         {
             InitializeComponent();
+            this.Paint += Form1_Paint;
         }
-
+        
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics; //The System.Drawing.Graphics object used to paint. The System.Drawing.Graphics
@@ -26,8 +28,8 @@ namespace REGION
                                              //it with the specified background color
 
             //создаем два прямоугольника
-            Rectangle rect1 = new Rectangle(40, 100, 140, 140);
-            Rectangle rect2 = new Rectangle(100, 100, 140, 140);
+            Rectangle rect1 = new Rectangle(40, 40, 250, 250);
+            Rectangle rect2 = new Rectangle(100, 100, 300, 200);
 
             // создаем два региона
 
@@ -49,6 +51,23 @@ namespace REGION
 
             //и заливаем ее зеленым цветом
             g.FillRegion(Brushes.Lime, rgn1);
+
+
+            Rectangle rect3 = new Rectangle(475, 225, 250, 150);            
+            g.DrawRectangle(Pens.Brown, 450, 200, 300, 200);
+            g.DrawEllipse(Pens.Green, rect3);
+            Region reg3 = new Region(rect3);
+            GraphicsPath path = new GraphicsPath();
+            path.AddBezier(325, 112, 315, 537, 44, 500, 25, 400);
+            g.FillPath(Brushes.Yellow, path);
+
+            GraphicsPath path2 = new GraphicsPath();
+            path2.AddEllipse(475, 225, 250, 150);
+            reg3.Xor(path2);
+            g.FillRegion(Brushes.Violet, reg3);
+
+
+
             g.Dispose();
         }
     }
