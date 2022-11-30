9b2e0a58b0cb935ee0375e80d901e699cd659f8e
Sergey Evseev <sevseev@gmail.com>
2022-11-30 03:48:12 +0300

    befor and after transform

--- COORDINATES/Form1.cs
+++ COORDINATES/Form1.cs
@@ -27,12 +27,16 @@ namespace COORDINATES
         {
             Graphics g = e.Graphics;
 
-            //сдвигаем начало координат страницы
-            g.TranslateTransform(40, 50);
+            //рисуем тестовую линию до смещения координат
+            g.DrawLine(new Pen(Brushes.DarkOrange, 10), 0, 0, 400, 350);
+            g.DrawLine(new Pen(Brushes.DarkGreen, 5), 300, 0, 450, 350);
 
-            //рисуем тестовую линию
+            //сдвигаем начало координат страницы
+            g.TranslateTransform(100, 50);
+
+            //рисуем тестовую линию после смещения 
             g.DrawLine(new Pen(Brushes.DarkSeaGreen, 10), 0, 0, 400, 350);
-            g.DrawLine(new Pen(Brushes.DarkRed, 5), 200, 0, 400, 350);
+            g.DrawLine(new Pen(Brushes.DarkRed, 5), 300, 0, 450, 350);
         }
     }
 }
