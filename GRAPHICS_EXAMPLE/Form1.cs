133c1e4f005aaef9fe5551a978b515dc0816a649
Sergey Evseev <sevseev@gmail.com>
2022-12-02 14:54:03 +0300

    MakeCaptionOnPicture

--- GRAPHICS_EXAMPLE/Form1.cs
+++ GRAPHICS_EXAMPLE/Form1.cs
@@ -17,28 +17,36 @@ namespace GRAPHICS_EXAMPLE
             InitializeComponent();
             this.Paint += new PaintEventHandler(Form1_Paint);
             
+            
         }
-        //СПОСОБЫ ПОЛУЧЕНИЯ ОБЪЕКТА GRAPHICS
+        //СПОСОБЫ ПОЛУЧЕНИЯ ОБЪЕКТА GRAPHICS///
 
         //1) получение объекта Graphics из входного параметра PaintEventsArgs
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
 
-            Font f = new Font("Yandex Sans Display", 45, FontStyle.Italic);
-            g.DrawString("Hello SATAN", f, Brushes.DarkMagenta, 100, 100);
+            Font f = new Font("Arial", 60, FontStyle.Italic);
+            g.DrawString("Arial", f, Brushes.DarkMagenta, 100, 100);
         }
 
 
-        //2)переопределение виртуального метода OnPaint базового класса Control
+        //2)переопределением виртуального метода OnPaint базового класса Control
         //protected override void OnPaint(PaintEventArgs e)
         //{
         //    base.OnPaint(e);
         //    Graphics g = e.Graphics;
-        //    Font f = new Font("Yandex Sans Display", 45, FontStyle.Italic);
-        //    g.DrawString("Hello SATAN", f, Brushes.DarkMagenta, 100, 100);
+        //    Font f = new Font("Yandex Sans Display", 45, FontStyle.Bold);
+        //    g.DrawString("Yandex Sans Display", f, Brushes.DarkMagenta, 180, 200);
         //}
-                
 
+        //3) вызовом метода CreateGraphics переопределенного метода OnPaint
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Graphics g = this.CreateGraphics();
+            Font f = new Font("Source Code Pro Semibold", 45, FontStyle.Bold);
+            g.DrawString("Hello SATAN", f, Brushes.DarkMagenta, 100, 300);
+        }
     }
 }
---
+++ GRAPHICS_EXAMPLE2/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GRAPHICS_EXAMPLE2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            buttonSave.Click += buttonSave_Click;
+
+            //this.Paint += Form1_Paint2;
+            this.Paint += Form1_Paint;
+            
+        }
+
+        //получение объекта Graphics с помощью объекта Image 
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            try //блок try catch на слуай отсутствия файла
+            {
+                Bitmap myBitmap = new Bitmap("rose.bmp");
+                //получаем объект Graphics ГОТОВЫЙ ИЗ ФАЙЛА
+                Graphics gFromImage = Graphics.FromImage(myBitmap);
+                //создаем объект font 
+                Font f = new Font("Times New Roman", 40, FontStyle.Bold | FontStyle.Italic);
+                //создаем надпись
+                string helloStr = "Kate My Love";
+
+                //измеряем строку методом MeasureString
+                SizeF sz = gFromImage.MeasureString(helloStr, f);
+
+                //пишем строку в координатах 10 10
+                gFromImage.DrawString(helloStr, f, Brushes.Aqua, 10, 10);
+
+                //рисуем прямоугольник размером с текст в координатах 10 10
+                                       //(Pen pen), float X, float Y, float width, float height                                       
+                gFromImage.DrawRectangle(new Pen(Color.Red, 3), 10.0F, 10.0F, sz.Width, sz.Height);
+                //сохраняем новое изображение на диск
+                myBitmap.Save(@"NewRose.bmp");
+                Rectangle regionRec = new Rectangle(new Point(0, 0), myBitmap.Size);
+                myBitmap.Dispose();
+                gFromImage.Dispose();
+                //метод принудительной перерисовки клиентской области
+                //в габаритах и локацией рисунка
+                this.Invalidate(regionRec);
+            }
+            catch { }            
+        }
+
+        //обработчик события Paint
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            try
+            {
+                Bitmap myBitmap = new Bitmap("NewRose.bmp");
+                Graphics g = e.Graphics;
+                g.DrawImage(myBitmap, 0, 0, 600, 400);
+                myBitmap.Dispose();
+                g.Dispose();
+            }
+            catch { }
+        }
+        //просто моя отрисовка по заданным параметрам
+        private void Form1_Paint2(object sender, PaintEventArgs e)
+        {
+            Bitmap myBitmap = new Bitmap("rose.bmp");
+            //получаем объект Graphics 
+            Graphics g = e.Graphics;
+            //создаем объект font 
+            Font f = new Font("Times New Roman", 40, FontStyle.Bold | FontStyle.Italic);
+            //создаем надпись
+            string helloStr = "Kate My Love";
+                  
+            g.DrawImage(myBitmap, this.ClientRectangle);
+            g.DrawString(helloStr, f, Brushes.DarkOrchid, 200, 10);
+        }
+    }
+}
